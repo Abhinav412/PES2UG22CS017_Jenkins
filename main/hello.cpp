fc8a97d4b1a2b26b6c02f0e72f1d26905339e9e1
Abhinav <112683325+Abhinav412@users.noreply.github.com>
2025-03-10 16:21:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,5 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "Abhinav has successfully built and run this  \n";
   cout << "Second change \n";
-  cout << "Third change \n";
   return 0;
 }
